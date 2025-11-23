bb9f63f4f13292472a9c3a8075b507dd2d31696f
the_poorvaj_567 <gowdapoorvaj8@gmail.com>
2025-11-23 20:51:36 +0530

    Add files via upload

---
+++ 2-script.js
@@ -0,0 +1,40 @@
+console.log("----------Datatypes--------");
+
+console.log("1.Numbers");
+let numInt = 34; //intiger datatype
+console.log("type of numInt = " + typeof(numInt));
+console.log(" numInt = " + numInt);
+
+let numFloat = 35.4; //floating datatype;
+console.log(" numFloat = " + numFloat);
+
+console.log("2.String");
+// sequence of charecters inside a quotes.(" " or ' ' or ` `) 
+let name = "poorvaj";
+console.log(" name = " + name);
+
+console.log("3. boole");
+let boo = true;
+console.log(typeof(boo));
+console.log("4.undifined");
+let un;
+console.log("type of un = " + typeof(un));
+
+console.log("5.NULL");
+let a = null;
+console.log(" type of a =" + typeof(a));
+
+console.log("6.object");
+let ob = { name: "poorvaj ", age: "19" }
+console.log(ob);
+console.log("type of ob = " + typeof(ob));
+
+console.log("6.arrays");
+let arr = ["name", "poorvaj ", "age", "19"]
+console.log(arr);
+console.log("type of arr = " + typeof(ob));
+
+console.log("6.symbole");
+let sy = Symbol("name");
+console.log(sy);
+console.log("type of arr = " + typeof(sy));
