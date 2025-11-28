77f4e390f43a46a082d4c93826374f8c65075a35
the_poorvaj_567 <gowdapoorvaj8@gmail.com>
2025-11-28 21:39:27 +0530

    Add files via upload

---
+++ script-4-day.js
@@ -0,0 +1,23 @@
+console.log("day 4 of learning java script");
+
+console.log("-----loops------");
+console.log("for loop");
+let a = 45;
+for (let i = 0; i < 50; i++) {
+    console.log("a= ", i);
+}
+
+console.log("while loop")
+while (a < 58) {
+    console.log('a is less than 45 ');
+    a++;
+
+}
+
+console.log("do while");
+let i = 0;
+do {
+
+    console.log("a= ", i, )
+    i++;
+} while (i < 56);
