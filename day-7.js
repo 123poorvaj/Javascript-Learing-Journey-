356ec6bddc7e0e137dd44d7ff9eb1218a2557f0b
the_poorvaj_567 <gowdapoorvaj8@gmail.com>
2025-12-01 20:44:51 +0530

    Add files via upload

---
+++ day-7.js
@@ -0,0 +1,8 @@
+//let name = prompt("enter your name");
+//console.log(name);
+
+function functionname() {
+    let user = document.getElementById("num1").value;
+    document.getElementById("result").innerHTML = user;
+    console.log(user);
+}
