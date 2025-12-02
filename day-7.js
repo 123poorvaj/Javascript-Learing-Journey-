58e9bf61c62e02082af5a8e016d0088b522e1bcb
the_poorvaj_567 <gowdapoorvaj8@gmail.com>
2025-12-03 00:14:30 +0530

    Add files via upload

--- day-7.js
+++ day-7.js
@@ -2,7 +2,36 @@
 //console.log(name);
 
 function functionname() {
-    let user = document.getElementById("num1").value;
-    document.getElementById("result").innerHTML = user;
-    console.log(user);
+    var user = parseInt(document.getElementById("num1").value);
+    var user2 = parseInt(document.getElementById("num2").value);
+    var sum = user + user2;
+    document.getElementById("result").innerHTML = sum;
+}
+
+function add() {
+    let number1 = parseInt(document.getElementById("num1").value);
+    let number2 = parseInt(document.getElementById("num2").value);
+    let sum = number1 + number2;
+    document.getElementById("result").innerHTML = sum;
+}
+
+function substract() {
+    let number1 = parseInt(document.getElementById("num1").value);
+    let number2 = parseInt(document.getElementById("num2").value);
+    let sum = number1 - number2;
+    document.getElementById("result").innerHTML = sum;
+}
+
+function divison() {
+    let number1 = parseInt(document.getElementById("num1").value);
+    let number2 = parseInt(document.getElementById("num2").value);
+    let sum = number1 / number2;
+    document.getElementById("result").innerHTML = sum;
+}
+
+function multiplay() {
+    let number1 = parseInt(document.getElementById("num1").value);
+    let number2 = parseInt(document.getElementById("num2").value);
+    let sum = number1 * number2;
+    document.getElementById("result").innerHTML = sum;
 }
