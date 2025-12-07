dd06771f9d54d8a929dc50e4f63fa384a6009753
the_poorvaj_567 <gowdapoorvaj8@gmail.com>
2025-12-07 19:28:51 +0530

    Add files via upload

---
+++ 08 script.js
@@ -0,0 +1,18 @@
+console.log("object and propertice");
+
+let obj = {
+    fristname: "poorvaj",
+    lastname: "m gowa",
+    work() { return " object and class"; },
+    id: 145,
+    cyber: {
+        video: ['ethical haking', 'king'],
+        cours: "full stack developer",
+    }
+}
+let a = obj.cyber;
+console.log(a);
+
+console.log(obj.work());
+console.log(obj.fristname);
+console.log(obj.lastname);
