c1fa6cb8bd88c333cab57adf42f81ab1c766353f
the_poorvaj_567 <gowdapoorvaj8@gmail.com>
2025-12-19 20:40:58 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -1,37 +1,18 @@
-console.log("----day-3----");
-console.log("operators");
+let number = document.getElementById("num");
+let addbtn = document.getElementById("addbtn");
+let subbtn = document.getElementById("subbtn");
+let num = 0;
 
-console.log("1.Aritmetic operators");
-let a = 2;
-let b = 1;
-console.log("addition operater + :", a + b);
-console.log("substraction operater - :", a - b);
-console.log("multipicatin operater * :", a * b);
-console.log("division operater / :", a / b);
-console.log("modules operater % :", a % b);
-console.log("power operater ** :", a ** b);
+addbtn.addEventListener("click", () => {
+    num++;
+    number.innerHTML = num;
+});
 
-console.log("2.comparison operators");
-
-console.log("Equals ", a == b);
-console.log("threebale equal", a === b);
-console.log("greater than : ", a > b);
-console.log("lessthan than : ", a < b);
-console.log("greaterthan or equalt :", a >= b);
-console.log("lessthan or equalt: ", a <= b);
-console.log("not equalt", a != b);
-
-console.log("3.logical operater");
-console.log("logical and &&", a && b);
-console.log("logic OR:", a || b);
-console.log("logic NOT b:", !b);
-console.log("logic NOT a:", !a);
-
-console.log("4.Bitwise operater");
-console.log("Bitwise and & ", a & b);
-console.log("bitwise OR (|) :", a || b);
-console.log("bitwise XOR (^) : ", a ^ b);
-console.log("Bitwise Not ~ :", ~a);
-console.log("Bitwise right shift >>:" >> a);
-console.log("Bitwise left shift <<:" << a);
-console.log("Bitwise unasigned rigth shift >>>:" >>> a);
+subbtn.addEventListener('click', () => {
+    if (num > 0) {
+        num--;
+        number.innerHTML = num;
+    } else {
+        number.textContent = "0 is least Count!you cann't decrease";
+    }
+})
