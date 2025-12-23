2a075fee301d22ae4be25934cdf2f61e2fb03acc
the_poorvaj_567 <gowdapoorvaj8@gmail.com>
2025-12-23 19:15:10 +0530

    Add files via upload

--- clalculatebmi.js
+++ clalculatebmi.js
@@ -1,20 +1,45 @@
-const { useOptimistic } = require("react");
+  let inputh = document.getElementById("input-h");
+  let inputw = document.getElementById("input-w");
+  let outh = document.getElementById("outh");
+  let outw = document.getElementById("outw");
+  let result = document.getElementById("result");
+  let weight = 0;
+  let height = 0;
+  inputh.addEventListener("mousedown", (event) => {
+      outheight(event);
+      inputh.addEventListener("mousemove", outheight);
 
-let inputw = document.getElementById("input-w");
-let inputh = document.getElementById("input-h");
-let outputw = document.getElementById("out-w");
-let outputh = document.getElementById("out-h");
 
-let w = 40;
-let height = 40;
-inputh.addEventListener("mousedown", (event) => {
-    updateh(event);
-    inputh.addEventListener("mousemove", updateh);
+  });
 
-})
+  inputw.addEventListener("mousedown", (event) => {
+      outweight(event);
+      inputw.addEventListener("mousemove", outweight);
+  });
 
-function updateh(event) {
-    let h = event.target.value;
-    height = h;
-    outputh.innerHTML = `${height} cm`;
-}
+  inputh.addEventListener('mouseup', () => {
+      inputh.removeEventListener('mousemove', outheight);
+  })
+
+  inputw.addEventListener("mouseup", () => {
+      inputw.removeEventListener("mousemove", outweight);
+  })
+
+  function outheight(event) {
+      let h = event.target.value;
+      height = h;
+      outh.innerText = `${height}cm`;
+      rrr();
+  }
+
+  function outweight(evet) {
+      let w = evet.target.value;
+      weight = w;
+      outw.innerHTML = `${weight}kg`;
+      rrr();
+  }
+
+  function rrr() {
+      let bmi = parseFloat(weight / ((height / 100) * (height / 100))).toFixed(2);
+      result.innerHTML = `${bmi}`;
+  }
